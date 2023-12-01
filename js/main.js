6d3fc78f05ff36242aef336923e0944d8cf927aa
goziyev <goziyevmuhammadgozi@gmail.com>
2023-12-01 14:14:39 +0500

    task completed

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let a = +prompt("Marhamat o'zingizda bor pulni kiriting ");
+let b = (11000.34 * 750);
+let yevro = (12354.03 * 120)
+let total = a - (b + yevro);
+ let res;
+if (total > 0 ) {
+    res = "Oq yo'l Alisher"
+}
+else{
+    res = "Alisher ozgina sabr qilish kerak bo'lar ekan"
+}
+console.log(res);
